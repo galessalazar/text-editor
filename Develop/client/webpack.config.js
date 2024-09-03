52dc3c01604dc809907e33e5a2c094ce2c51cb72
Al Bundy <gale.s.salazar22@gmail.com>
2024-09-03 17:12:56 -0500

    updated webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        // this title will appear in the browser tab you can name it whatever is relevant
+        title: 'Text Editor'
+      }),
+
+      // think offline capability
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Save all your notes here!',
+        background_color: 'blue',
+        theme_color: 'blue',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            // a new assets diretory should be created with a icons file that will store the new icon files created, this wont happen until i npm run build i think
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+
+      }),
     ],
 
     module: {
+
       rules: [
-        
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },  
       ],
     },
   };
 };
+// npm run build when done
