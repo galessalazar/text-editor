46ae35d677c5dd9de458a4732d7cf548c5876609
Al Bundy <gale.s.salazar22@gmail.com>
2024-09-05 11:12:37 -0500

    updated relative path in server.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,10 +1,12 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// sw will attempt to server content from cache first
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// the method takes an array of URLs to precache and the self holds the list to precache
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -14,11 +16,12 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
+      // this is 30 days 24 hours 60 mins in an hour and 60 secs in a min
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
-
+// makes urls like index.html and root preload into cache to make available offline 
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
@@ -27,4 +30,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- Develop/server/routes/htmlRoutes.js
+++ Develop/server/routes/htmlRoutes.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    // i changed the path and added ../ 
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
   );
